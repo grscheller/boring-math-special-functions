251a99f795cc845c46ca7ac15bc1e4d7c0895b64
grscheller <geoffrey@scheller.com>
2026-04-01 12:31:02 -0600

    Added initial docstring to new __init__.py functions.

--- src/boring_math/special_functions/__init__.py
+++ src/boring_math/special_functions/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2023-2026 Geoffrey R. Scheller
+# Copyright 2025-2026 Geoffrey R. Scheller
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,9 +16,10 @@
 Mathematical special functions
 ------------------------------
 
-.. admonition:: Special Functions for Mathematics and Physics
+.. admonition:: Special functions for mathematics and physics
 
-Implementations of classical special functions.
+    Implementations for classical complex and real valued special
+    functions.
 
 """
 
